41ae3111651c28ce0bf38b1bc1a4b5e5b5c2357a
guilhermemoraes1 <guilherme.morais.54540218@gmail.com>
2025-03-11 20:37:40 -0300

    adicionar carrossel

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,51 @@
 import { Container } from 'react-bootstrap';
 import PropriedadesCard from './PropriedadesCard';
 import '../App.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 const Main = () => {
   return (
     <main >
       <Container className="mt-2">
-        {/* Propriedades */}
+      <h1 style={{margin: '3rem', textAlign: 'center'}}>Carrossel</h1>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.unsplash.com/photo-1726820431575-500cbc6f7815?q=80&w=1467&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://plus.unsplash.com/premium_photo-1741374967692-2a7ab44af968?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.unsplash.com/photo-1648516148105-2a24449fba39?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
         <h1 style={{margin: '3rem', textAlign: 'center'}}>Notícias</h1>
         <PropriedadesCard />
       </Container>
